e60f0930a39c395fc629512b0f5c3335faf35021
abid <abidhussain@pursuit.org>
2019-07-04 16:47:34 -0400

    test api

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,13 +1,47 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import axios from "axios";
+const url = "https://ws-api.iextrading.com/1.0/last";
+const socket = require("socket.io-client")(url);
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello world</p>
-    </div>
-  );
+// socket.on("connect", () => {
+//   socket.emit("subscribe", "snap");
+// });
+// socket.on("message", message => console.log(JSON.parse(message).price));
+
+class App extends React.Component {
+  state = {
+    input: "",
+    name: "",
+    price: ""
+  };
+
+  inputHandler = e => {
+    e.preventDefault();
+    this.setState({
+      input: e.target.value
+    });
+  };
+
+  getStockPrice = () => {
+    console.log("fired", this.state.input);
+    // let sym = this.state.input;
+    socket.on("connect", () => {
+      socket.emit("subscribe", "snap");
+    });
+    socket.on("message", message => console.log(JSON.parse(message).price));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hello world</p>
+        <input onChange={this.inputHandler} type="text" />
+        <button onClick={this.getStockPrice}>Get Price</button>
+        <p>{this.state.name ? this.state.name : null}</p>
+        <p>{this.state.price ? this.state.price : null}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
